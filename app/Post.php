b0c1e33df0fba143aff30b644d664f761fc6df93
franckalain <k.franckalain@gmail.com>
2019-11-08 15:43:24 +0000

    Markdown support to the blog post

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use GrahamCampbell\Markdown\Facades\Markdown;
+
 use Carbon\Carbon;
 
 class Post extends Model
@@ -33,6 +35,16 @@ class Post extends Model
     public function getDateAttribute($value)
     {
         return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();
+    }
+
+    public function getBodyHtmlAttribute($value)
+    {
+        return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL;
+    }
+
+    public function getExcerptHtmlAttribute($value)
+    {
+        return $this->excerpt ? Markdown::convertToHtml(e($this->excerpt)) : NULL;
     }
 
     public function scopeLatestFirst($query)
