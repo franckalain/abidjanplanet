b4b96e5484c6278bbe10e242beb29674c9a6cf22
franckalain <k.franckalain@gmail.com>
2019-11-13 14:22:41 +0000

    image extension and thumbnail

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 
 use App\Category;
 
+use App\Post;
+
 class ComposerServiceProvider extends ServiceProvider
 {
     /**
@@ -25,12 +27,19 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer('sidebar', function($view){
+        view()->composer('layouts.sidebar', function($view){
             $categories = Category::with(['posts' => function($query){
                 $query->published();
             }])->orderBy('title', 'asc')->get();
 
             return $view->with('categories', $categories);
         });
+
+        view()->composer('layouts.sidebar', function($view){
+            $popularPosts = Post::published()->popular()->take(3)->get();
+            return $view->with('popularPosts', $popularPosts);
+        });
+
+
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,27 +25,7 @@
 
             </div>
 
-            <div class="row">
-            <div class="col-md-8">
-            @foreach($WebProgramming->posts()->orderBy('created_at', 'desc')->take(4)->get() as $post)
-                    <div class="post-item ver1 overlay video" data-videourl="{{$post->excerpt}}" data-videosite="youtube">
-                        <div class="images">@if($post->image_url)
-                    <div class="post-item-image">
-                        <img src="{{$post->image_url}}" alt="{{$post->title}}">
-                    </div>
-                    @endif</div>
-                        <div class="text">
-                            <h2><span><a href="{{route('show', $post->slug)}}">{{$post->title}}</a></span></h2>
-                            <h2><span><a href="{{route('show', $post->slug)}}">{{$post->excerpt}}</a></span></h2>
-                        </div>
-                    </div>
 
-                    <!-- End item -->
-                @endforeach
-
-
-
-            </div>
             @include('layouts.sidebar')
         </div>
     </div>
